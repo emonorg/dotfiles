e9c398b88f3e03b6ec2d5384c2fb82533eb15462
Administrator <emonoorg@gmail.com>
2025-12-05 18:16:39 +0100

    fix: wezterm dir struct issue

--- terminal-em/.config/wezterm/wezterm.lua
+++ terminal-em/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Font configuration
-config.font = wezterm.font('Roboto Mono')
+config.font = wezterm.font('JetBrains Mono')
 config.font_size = 14.0
 
 -- Color scheme
