bd51e1cb2dec358d04b238fbf2dc84da419ebe19
Emad Mamaghani <emad.mamaghani@nn.nl>
2025-12-05 17:42:25 +0100

    feat: add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+-- Font configuration
+config.font = wezterm.font('Roboto Mono')
+config.font_size = 14.0
+
+-- Color scheme
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+
+-- Window configuration
+config.window_background_opacity = 1.0
+config.window_decorations = "RESIZE"
+
+-- Tab bar
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+
+-- Scrollback
+config.scrollback_lines = 10000
+
+return config
