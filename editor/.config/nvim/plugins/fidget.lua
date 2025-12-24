5a6bdf33ee8d2f827aa1c7022c01eae3c7894f5b
Emad Mamaghani <emad.mamaghani@nn.nl>
2025-12-24 14:13:39 +0100

    feat: replace fidget.nvim with noice.nvim for enhanced LSP progress notifications

--- editor/.config/nvim/plugins/fidget.lua
+++ editor/.config/nvim/plugins/fidget.lua
@@ -1,12 +1,59 @@
--- Fidget: LSP progress notifications
+-- noice.nvim: Modern UI for messages, cmdline and popups
 return {
-	"j-hui/fidget.nvim",
-	event = "LspAttach",
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
 	opts = {
-		notification = {
-			window = {
-				winblend = 0,
-				border = "none",
+		lsp = {
+			progress = {
+				enabled = true,
+				format = "lsp_progress",
+				format_done = "lsp_progress_done",
+				throttle = 1000 / 30, -- frequency to update lsp progress message
+				view = "mini",
+			},
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+		presets = {
+			bottom_search = true, -- use a classic bottom cmdline for search
+			command_palette = false, -- position the cmdline and popupmenu together
+			long_message_to_split = true, -- long messages will be sent to a split
+			inc_rename = false, -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = true, -- add a border to hover docs and signature help
+		},
+		views = {
+			mini = {
+				position = {
+					row = -2,
+					col = "100%",
+				},
+				size = {
+					width = "auto",
+					height = "auto",
+				},
+				border = {
+					style = "none",
+				},
+				win_options = {
+					winblend = 0,
+				},
+			},
+		},
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "written",
+				},
+				opts = { skip = true },
 			},
 		},
 	},
